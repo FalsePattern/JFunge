a010fc6530ad6269ba5a85f6d41119030db2cd0a
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-10-22 20:58:37 +0200

    rebrand handprint to "JFUN"

--- src/main/java/com/falsepattern/jfunge/Globals.java
+++ src/main/java/com/falsepattern/jfunge/Globals.java
@@ -3,6 +3,6 @@ package com.falsepattern.jfunge;
 public class Globals {
     public static final String VERSION = "1.0.1";
     public static final int FUNGE_VERSION = 1 * 256 * 256 + 0 * 256 + 1;
-    public static final int HANDPRINT = 0xfa15e9a7; //"falsepat"
+    public static final int HANDPRINT = 0x74_70_85_78; //"JFUN"
     public static final String LICENSE = "JFunge - A standard-conforming Befunge-98 and Trefunge-98 interpreter\n" + "Copyright (C) 2022 FalsePattern\n" + "\n" + "This program is free software: you can redistribute it and/or modify\n" + "it under the terms of the GNU General Public License as published by\n" + "the Free Software Foundation, either version 3 of the License, or\n" + "(at your option) any later version.\n" + "\n" + "This program is distributed in the hope that it will be useful,\n" + "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" + "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n" + "GNU General Public License for more details.\n" + "\n" + "You should have received a copy of the GNU General Public License\n" + "along with this program. If not, see <https://www.gnu.org/licenses/>.";
 }

0548c311f15c47cd47a150c7585735aae30d0c49
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-12-22 19:47:57 +0100

    implement BASE

--- src/main/java/com/falsepattern/jfunge/interpreter/instructions/Funge98.java
+++ src/main/java/com/falsepattern/jfunge/interpreter/instructions/Funge98.java
@@ -2,6 +2,7 @@ package com.falsepattern.jfunge.interpreter.instructions;
 
 import com.falsepattern.jfunge.Globals;
 import com.falsepattern.jfunge.interpreter.ExecutionContext;
+import com.falsepattern.jfunge.interpreter.instructions.fingerprints.BASE;
 import com.falsepattern.jfunge.interpreter.instructions.fingerprints.FPDP;
 import com.falsepattern.jfunge.interpreter.instructions.fingerprints.FPSP;
 import com.falsepattern.jfunge.interpreter.instructions.fingerprints.HRTI;
@@ -43,6 +44,7 @@ public class Funge98 implements InstructionSet {
 
     static {
         addFingerprint(_3DSP.INSTANCE);
+        addFingerprint(BASE.INSTANCE);
         addFingerprint(FPSP.INSTANCE);
         addFingerprint(FPDP.INSTANCE);
         addFingerprint(HRTI.INSTANCE);
---
+++ src/main/java/com/falsepattern/jfunge/interpreter/instructions/fingerprints/BASE.java
@@ -0,0 +1,57 @@
+package com.falsepattern.jfunge.interpreter.instructions.fingerprints;
+
+import com.falsepattern.jfunge.interpreter.ExecutionContext;
+import com.falsepattern.jfunge.interpreter.instructions.Fingerprint;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BASE implements Fingerprint {
+    public static final BASE INSTANCE = new BASE();
+
+    @Override
+    public int code() {
+        return 0x42415345;
+    }
+
+    @SneakyThrows
+    @Instr('B')
+    public static void printBinary(ExecutionContext ctx) {
+        ctx.output().write(Integer.toBinaryString(ctx.stack().pop()).getBytes());
+    }
+
+    @SneakyThrows
+    @Instr('H')
+    public static void printHex(ExecutionContext ctx) {
+        ctx.output().write(Integer.toHexString(ctx.stack().pop()).getBytes());
+    }
+
+    @Instr('I')
+    public static void readIntBase(ExecutionContext ctx) {
+        int base = ctx.stack().pop();
+        //Read input until non-digit character is encountered
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while (Character.isDigit(c = ctx.input(true))) {
+            sb.append((char) c);
+            ctx.input(false);
+        }
+        ctx.stack().push(Integer.parseInt(sb.toString(), base));
+    }
+
+    @SneakyThrows
+    @Instr('N')
+    public static void printInBase(ExecutionContext ctx) {
+        int base = ctx.stack().pop();
+        int value = ctx.stack().pop();
+        ctx.output().write(Integer.toString(value, base).getBytes());
+    }
+
+    @SneakyThrows
+    @Instr('O')
+    public static void printOctal(ExecutionContext ctx) {
+        ctx.output().write(Integer.toOctalString(ctx.stack().pop()).getBytes());
+    }
+
+}

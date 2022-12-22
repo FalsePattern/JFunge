c47cdf59fec1dbf326af85e83963a103717920cb
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-12-22 21:50:10 +0100

    Version 1.1.0

--- src/main/java/com/falsepattern/jfunge/Globals.java
+++ src/main/java/com/falsepattern/jfunge/Globals.java
@@ -1,7 +1,7 @@
 package com.falsepattern.jfunge;
 
 public class Globals {
-    public static final String VERSION = "1.0.1";
-    public static final int FUNGE_VERSION = 1 * 256 * 256 + 0 * 256 + 1;
+    public static final String VERSION = "1.1.0";
+    public static final int FUNGE_VERSION = 1 * 256 * 256 + 1 * 256 + 0;
     public static final int HANDPRINT = 0x74_70_85_78; //"JFUN"
 }
